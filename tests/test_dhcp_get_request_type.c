81f3e61536e885351da3a2361c1ec69915253c56
Clayton Craft <clayton@craftyguy.net>
2021-09-09 22:55:59 -0700

    rename header->message, use correct option length
    I should have used 'message' all along, since that's what the relevant
    RFC calls this. The 'header' is the first 236 bytes before the
    'options'.
    
    The min/max size for the message are added as macros, and the length of
    the options was adjusted based on further reading of RFC2131.
    
    Lastly, this fixes an out-of-bounds access that was not caught by
    -Wall -Wextra, but only showed up with -Os

--- tests/test_dhcp_get_request_type.c
+++ tests/test_dhcp_get_request_type.c
@@ -2,112 +2,111 @@
 #include <string.h>
 #include "../server.h"
 
-typedef int (*header_getter)(dhcp_header *h);
+typedef int (*message_getter)(dhcp_message *m);
 
 typedef struct {
 	char *name;
-	header_getter in_header;
+	message_getter in_message;
 	int expected;
 } testcase;
 
-int dhcp_get_request_type(dhcp_header *request, ssize_t request_len);
+int dhcp_get_request_type(dhcp_message *request, ssize_t request_len);
 
 // Sets op code, message type, option magic and option message flag
-void set_required_bits(uint8_t *h) {
+void set_required_bits(uint8_t *m) {
 	int o = DHCP_HEADER_SIZE;
-	h[0] = BOOTREQUEST;
-	h[1] = 1;  // ethernet
-	h[o] = DHCP_OPTION_MAGIC >> 24;
-	h[o+1] = (0xFF0000 & DHCP_OPTION_MAGIC) >> 16;
-	h[o+2] = (0x00FF00 & DHCP_OPTION_MAGIC) >> 8;
-	h[o+3] = 0x0000FF & DHCP_OPTION_MAGIC;
+	m[0] = BOOTREQUEST;
+	m[1] = 1;  // ethernet
+	m[o] = DHCP_OPTION_MAGIC >> 24;
+	m[o+1] = (0xFF0000 & DHCP_OPTION_MAGIC) >> 16;
+	m[o+2] = (0x00FF00 & DHCP_OPTION_MAGIC) >> 8;
+	m[o+3] = 0x0000FF & DHCP_OPTION_MAGIC;
 }
 
-int get_tiny_request_header(dhcp_header *header) {
+int get_tiny_request_message(dhcp_message *message) {
 	int o = DHCP_HEADER_SIZE;
-	uint8_t h[243] = {0};
-	set_required_bits((uint8_t*)&h);
-	h[o+4] = DHCP_MESSAGE_TYPE;
-	h[o+5] = 1; 
-	h[o+6] = DHCP_REQUEST; 
-	h[o+7] = 0xFF;
+	uint8_t m[DHCP_MESSAGE_SIZE_MIN] = {0};
+	set_required_bits((uint8_t*)&m);
+	m[o+4] = DHCP_MESSAGE_TYPE;
+	m[o+5] = 1; 
+	m[o+6] = DHCP_REQUEST; 
+	m[o+7] = 0xFF;
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
-int get_tiny_discover_header(dhcp_header *header) {
+int get_tiny_discover_message(dhcp_message *message) {
 	int o = DHCP_HEADER_SIZE;
-	uint8_t h[243] = {0};
-	set_required_bits((uint8_t*)&h);
-	h[o+4] = DHCP_MESSAGE_TYPE;
-	h[o+5] = 1; 
-	h[o+6] = DHCP_DISCOVER; 
-	h[o+7] = 0xFF;
+	uint8_t m[DHCP_MESSAGE_SIZE_MIN] = {0};
+	set_required_bits((uint8_t*)&m);
+	m[o+4] = DHCP_MESSAGE_TYPE;
+	m[o+5] = 1; 
+	m[o+6] = DHCP_DISCOVER; 
+	m[o+7] = 0xFF;
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
-int get_no_request_header_0(dhcp_header *header) {
-	uint8_t h[432] = {0};
-	set_required_bits((uint8_t*)&h);
+int get_no_request_message_0(dhcp_message *message) {
+	uint8_t m[432] = {0};
+	set_required_bits((uint8_t*)&m);
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
-int get_no_request_header_2(dhcp_header *header) {
-	uint8_t h[347] = {2};
-	set_required_bits((uint8_t*)&h);
+int get_no_request_message_2(dhcp_message *message) {
+	uint8_t m[347] = {2};
+	set_required_bits((uint8_t*)&m);
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
 // Before the end of a max-sized options buffer, add a len byte that tells the
 // parser to read past the end of the buffer
-int get_broken_header_overflow(dhcp_header *header) {
+int get_broken_message_overflow(dhcp_message *message) {
 	int o = DHCP_HEADER_SIZE;
-	uint8_t h[548] = {0};
-	set_required_bits((uint8_t*)&h);
-	h[o+310] = DHCP_MESSAGE_TYPE;
-	h[o+311] = 1; 
-	h[o+312] = DHCP_REQUEST; 
+	uint8_t m[DHCP_MESSAGE_SIZE_MAX] = {0};
+	set_required_bits((uint8_t*)&m);
+	m[o+338] = DHCP_MESSAGE_TYPE; 
+	m[o+339] = 1; 
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
-int get_large_discover_header(dhcp_header *header) {
+int get_large_discover_message(dhcp_message *message) {
 	int o = DHCP_HEADER_SIZE;
-	uint8_t h[548] = {0};
-	set_required_bits((uint8_t*)&h);
-	h[o+308] = DHCP_MESSAGE_TYPE;
-	h[o+309] = 1; 
-	h[o+310] = DHCP_DISCOVER; 
-	h[o+311] = 0xff; 
+	uint8_t m[DHCP_MESSAGE_SIZE_MAX] = {0};
+	set_required_bits((uint8_t*)&m);
+	m[o+336] = DHCP_MESSAGE_TYPE;
+	m[o+337] = 1; 
+	m[o+338] = DHCP_DISCOVER; 
+	m[o+339] = 0xff; 
 
-	memcpy(header, &h, sizeof(h));
-	return sizeof(h);
+	memcpy(message, &m, sizeof(m));
+	return sizeof(m);
 }
 
 int main(void) {
 	int fails = 0;
 	testcase tests[] = {
-		{"DHCPREQUEST, small header", get_tiny_request_header, DHCP_REQUEST},
-		{"DHCPDISCOVER, small header", get_tiny_discover_header, DHCP_DISCOVER},
-		{"DHCPDISCOVER, large header", get_large_discover_header, DHCP_DISCOVER},
-		{"Options all padding", get_no_request_header_0, -1},
-		{"Options all 2's", get_no_request_header_2, -1},
-		{"Broken header, overflow", get_broken_header_overflow, -1},
+		{"DHCPREQUEST, small message", get_tiny_request_message, DHCP_REQUEST},
+		{"DHCPDISCOVER, small message", get_tiny_discover_message, DHCP_DISCOVER},
+		{"DHCPDISCOVER, large message", get_large_discover_message, DHCP_DISCOVER},
+		{"Options all padding", get_no_request_message_0, -1},
+		{"Options all 2's", get_no_request_message_2, -1},
+		{"Broken message, overflow", get_broken_message_overflow, -1},
 	};
 
 	for (int i=0; i<(int)(sizeof(tests) / sizeof(tests[0])); i++) {
 		testcase test = tests[i];
-		dhcp_header h = {0};
-		int size = test.in_header(&h);
-		int out = dhcp_get_request_type(&h, size);
+		dhcp_message m = {0};
+		int size = test.in_message(&m);
+		int out = dhcp_get_request_type(&m, size);
 		if (test.expected != out) {
 			fprintf(stderr, "dhcp_get_request_type (%s) failed: expected %d, got: %d\n",
 					test.name, test.expected, out);
