99degree/unudhcpd

commit 44cbc65c28992ad327c1ceee45b05e19b3d9c897

dhcp_is_valid_request: check for dhcp option magic

server.c
@@ -56,8 +56,7 @@ int dhcp_create_response(dhcp_config *config, dhcp_header *request, dhcp_header 
 	dhcp_response_options options = {0};
 
 	// DHCP magic cookie
-	uint8_t magic_cookie[4] = {0x63, 0x82, 0x53, 0x63};
-	memcpy(&options.magic, &magic_cookie, 4);
+	options.magic = DHCP_OPTION_MAGIC;
 
 	// DHCP message type
 	options.msg_type_option = DHCP_MESSAGE_TYPE;
@@ -182,6 +181,10 @@ int dhcp_is_invalid_request(dhcp_header *request, ssize_t request_len) {
 	}
 
 	if (request->op != BOOTREQUEST)
+		return 1;
+
+	uint32_t magic = (request->options[0] << 24) + (request->options[1] << 16) + (request->options[2] << 8) + request->options[3];
+	if (magic != DHCP_OPTION_MAGIC)
 		return 1;
 
 	// Minimum size for a DHCP DISCOVER/REQUEST seems to be:

server.h
@@ -24,6 +24,8 @@
 
 #define DHCP_HEADER_SIZE 236
 
+#define DHCP_OPTION_MAGIC 0x63825363
+
 // From: https://datatracker.ietf.org/doc/html/rfc2131#page-37
 typedef struct dhcp_header {
 	uint8_t op;
@@ -55,7 +57,7 @@ typedef struct dhcp_config {
 // Only options used for DHCP OFFER/ACK
 typedef struct dhcp_response_options {
 	// DHCP magic cookie
-	uint8_t magic[4];
+	uint32_t magic;
         // DHCP message type
         uint8_t msg_type_option;
         uint8_t msg_type_len;

tests/test_dhcp_is_invalid_request.c
@@ -5,6 +5,7 @@
 typedef struct {
 	uint8_t in_op;
 	uint8_t in_htype;
+	uint32_t in_option_magic;
 	int in_size;
 	int expected;
 } testcase;
@@ -15,22 +16,26 @@ int main(void) {
 	int fails = 0;
 	testcase tests[] = {
 		// op, hw type, & size OK
-		{1, 1, 270, 0},
+		{1, 1, DHCP_OPTION_MAGIC, 270, 0},
 		// invalid/unsupported op code
-		{10, 1, 270, 1},
-		{-127, 1, 270, 1},
-		{'\0', 1, 270, 1},
-		{'a', 1, 270, 1},
+		{10, 1, DHCP_OPTION_MAGIC, 270, 1},
+		{-127, 1, DHCP_OPTION_MAGIC, 270, 1},
+		{'\0', 1, DHCP_OPTION_MAGIC, 270, 1},
+		{'a', 1, DHCP_OPTION_MAGIC, 270, 1},
 		// invalid hw types
-		{1, 3, 270, 1},
-		{1, 'b', 270, 1},
+		{1, 3, DHCP_OPTION_MAGIC, 270, 1},
+		{1, 'b', DHCP_OPTION_MAGIC, 270, 1},
 		// too small
-		{1, 1, 42, 1},
-		{1, 1, 242, 1},
-		{1, 1, '\0', 1},
+		{1, 1, DHCP_OPTION_MAGIC, 42, 1},
+		{1, 1, DHCP_OPTION_MAGIC, 242, 1},
+		{1, 1, DHCP_OPTION_MAGIC, '\0', 1},
 		// too big
-		{1, 1, INT32_MAX, 1},
-		{1, 1, 577, 1},
+		{1, 1, DHCP_OPTION_MAGIC, INT32_MAX, 1},
+		{1, 1, DHCP_OPTION_MAGIC, 577, 1},
+		// bad magic
+		{1, 1, DHCP_OPTION_MAGIC - 1, 270, 1},
+		{1, 1, 0x73825363, 270, 1},
+		{1, 1, 0x825363, 270, 1},
 	};
 
 	for (int i=0; i<(int)(sizeof(tests) / sizeof(tests[0])); i++) {
@@ -38,6 +43,11 @@ int main(void) {
 		dhcp_header h = {0};
 		h.op = test.in_op;
 		h.htype = test.in_htype;
+		h.options[0] = test.in_option_magic >> 24;
+		h.options[1] = (0xFF0000 & test.in_option_magic) >> 16;
+		h.options[2] = (0x00FF00 & test.in_option_magic) >> 8;
+		h.options[3] = 0x0000FF & test.in_option_magic;
+
 		int out = dhcp_is_invalid_request(&h, test.in_size);
 		if (test.expected != out) {
 			fprintf(stderr, "dhcp_is_invalid_request (op: %d, hwtype: %d, size: %d) failed: expected %d, got: %d\n",

commit 94fa440ed573bd968fd8c0c4592045c06fb4bd43

server: don't use macro for magic
This fixes a regression in 44cbc65, which caused the server to respond
with invalid DHCP option magic due to byte order being incorrect.

server.c
@@ -56,7 +56,7 @@ int dhcp_create_response(dhcp_config *config, dhcp_message *request, dhcp_messag
 	dhcp_response_options options = {0};
 
 	// DHCP magic cookie
-	options.magic = DHCP_OPTION_MAGIC;
+	memcpy(&options.magic, &dhcp_option_magic, sizeof(options.magic));
 
 	// DHCP message type
 	options.msg_type_option = DHCP_MESSAGE_TYPE;
@@ -183,8 +183,12 @@ int dhcp_is_invalid_request(dhcp_message *request, ssize_t request_len) {
 	if (request->op != BOOTREQUEST)
 		return 1;
 
-	uint32_t magic = (request->options[0] << 24) + (request->options[1] << 16) + (request->options[2] << 8) + request->options[3];
-	if (magic != DHCP_OPTION_MAGIC)
+	uint8_t magic[4] = {
+		request->options[0],
+		request->options[1],
+		request->options[2],
+		request->options[3]};
+	if (memcmp(&magic, &dhcp_option_magic, sizeof(magic)) != 0)
 		return 1;
 
 	if (request_len < DHCP_MESSAGE_SIZE_MIN || request_len > DHCP_MESSAGE_SIZE_MAX)

server.h
@@ -29,7 +29,7 @@
 // 576 is max per RFC 2131 pg. 10, for un-extended message
 #define DHCP_MESSAGE_SIZE_MAX  576
 
-#define DHCP_OPTION_MAGIC 0x63825363
+static const uint8_t dhcp_option_magic[4] = {0x63, 0x82, 0x53, 0x63};
 
 // From: https://datatracker.ietf.org/doc/html/rfc2131#page-37
 typedef struct dhcp_message {
@@ -63,7 +63,7 @@ typedef struct dhcp_config {
 // Only options used for DHCP OFFER/ACK
 typedef struct dhcp_response_options {
 	// DHCP magic cookie
-	uint32_t magic;
+	uint8_t magic[4];
         // DHCP message type
         uint8_t msg_type_option;
         uint8_t msg_type_len;

tests/get_request_type_options_fuzzer.c
@@ -13,10 +13,7 @@ void set_required_bits(uint8_t *m) {
 	int o = DHCP_HEADER_SIZE;
 	m[0] = BOOTREQUEST;
 	m[1] = 1;  // ethernet
-	m[o] = DHCP_OPTION_MAGIC >> 24;
-	m[o+1] = (0xFF0000 & DHCP_OPTION_MAGIC) >> 16;
-	m[o+2] = (0x00FF00 & DHCP_OPTION_MAGIC) >> 8;
-	m[o+3] = 0x0000FF & DHCP_OPTION_MAGIC;
+	memcpy(&m[o], &dhcp_option_magic, sizeof(dhcp_option_magic));
 }
 
 

tests/test_dhcp_get_request_type.c
@@ -17,10 +17,7 @@ void set_required_bits(uint8_t *m) {
 	int o = DHCP_HEADER_SIZE;
 	m[0] = BOOTREQUEST;
 	m[1] = 1;  // ethernet
-	m[o] = DHCP_OPTION_MAGIC >> 24;
-	m[o+1] = (0xFF0000 & DHCP_OPTION_MAGIC) >> 16;
-	m[o+2] = (0x00FF00 & DHCP_OPTION_MAGIC) >> 8;
-	m[o+3] = 0x0000FF & DHCP_OPTION_MAGIC;
+	memcpy(&m[o], &dhcp_option_magic, sizeof(dhcp_option_magic));
 }
 
 int get_tiny_request_message(dhcp_message *message) {

tests/test_dhcp_is_invalid_request.c
@@ -5,7 +5,7 @@
 typedef struct {
 	uint8_t in_op;
 	uint8_t in_htype;
-	uint32_t in_option_magic;
+	uint8_t in_option_magic[4];
 	int in_size;
 	int expected;
 } testcase;
@@ -16,26 +16,26 @@ int main(void) {
 	int fails = 0;
 	testcase tests[] = {
 		// op, hw type, & size OK
-		{1, 1, DHCP_OPTION_MAGIC, 270, 0},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 270, 0},
 		// invalid/unsupported op code
-		{10, 1, DHCP_OPTION_MAGIC, 270, 1},
-		{-127, 1, DHCP_OPTION_MAGIC, 270, 1},
-		{'\0', 1, DHCP_OPTION_MAGIC, 270, 1},
-		{'a', 1, DHCP_OPTION_MAGIC, 270, 1},
+		{10, 1, {0x63, 0x82, 0x53, 0x63}, 270, 1},
+		{-127, 1, {0x63, 0x82, 0x53, 0x63}, 270, 1},
+		{'\0', 1, {0x63, 0x82, 0x53, 0x63}, 270, 1},
+		{'a', 1, {0x63, 0x82, 0x53, 0x63}, 270, 1},
 		// invalid hw types
-		{1, 3, DHCP_OPTION_MAGIC, 270, 1},
-		{1, 'b', DHCP_OPTION_MAGIC, 270, 1},
+		{1, 3, {0x63, 0x82, 0x53, 0x63}, 270, 1},
+		{1, 'b', {0x63, 0x82, 0x53, 0x63}, 270, 1},
 		// too small
-		{1, 1, DHCP_OPTION_MAGIC, 42, 1},
-		{1, 1, DHCP_OPTION_MAGIC, 242, 1},
-		{1, 1, DHCP_OPTION_MAGIC, '\0', 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 42, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 242, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, '\0', 1},
 		// too big
-		{1, 1, DHCP_OPTION_MAGIC, INT32_MAX, 1},
-		{1, 1, DHCP_OPTION_MAGIC, 577, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, INT32_MAX, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 577, 1},
 		// bad magic
-		{1, 1, DHCP_OPTION_MAGIC - 1, 270, 1},
-		{1, 1, 0x73825363, 270, 1},
-		{1, 1, 0x825363, 270, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 42, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, 242, 1},
+		{1, 1, {0x63, 0x82, 0x53, 0x63}, '\0', 1},
 	};
 
 	for (int i=0; i<(int)(sizeof(tests) / sizeof(tests[0])); i++) {
@@ -43,10 +43,7 @@ int main(void) {
 		dhcp_message m = {0};
 		m.op = test.in_op;
 		m.htype = test.in_htype;
-		m.options[0] = test.in_option_magic >> 24;
-		m.options[1] = (0xFF0000 & test.in_option_magic) >> 16;
-		m.options[2] = (0x00FF00 & test.in_option_magic) >> 8;
-		m.options[3] = 0x0000FF & test.in_option_magic;
+		memcpy(&m.options, &test.in_option_magic, sizeof(test.in_option_magic));
 
 		int out = dhcp_is_invalid_request(&m, test.in_size);
 		if (test.expected != out) {